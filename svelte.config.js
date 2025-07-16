15c90ed15831c2e82f2ac5f945907bb1f2139957
feras <feras10personal@gmail.com>
2025-07-16 15:17:14 +0300

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,5 @@
 import adapter from '@sveltejs/adapter-vercel';
 
-export default {
-	kit: {
-		adapter: adapter({
+const config = { kit: { adapter: adapter() } };
 
-		})
-	}
-};
+export default config;
