bbbb71913c7ed688ddd358f1d0b702568d63ce3b
feras <feras10personal@gmail.com>
2025-07-16 16:59:10 +0300

    Update header.svelte

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -4,13 +4,13 @@
 </script>
 
 <header class="bg-black py-1 px-4 shadow-md">
-  <img src={logo} alt="Logo" class="h-35 place-self-end pr-[10%]" />
+  <img src={logo} alt="Logo" class="h-35  pl-[10%]" />
 
   <div class="container mx-auto flex justify-between items-center relative text-white">
     {#if !user}
       <a
         href="/login"
-        class="absolute left-4 top -translate-y-17 text-white text-sm bg-white/30 px-3 py-1 rounded hover:bg-white/40 transition"
+        class="absolute right-4 top -translate-y-17 text-white text-sm bg-white/30 px-3 py-1 rounded hover:bg-white/40 transition"
       >
         تسجيل الدخول
       </a>
